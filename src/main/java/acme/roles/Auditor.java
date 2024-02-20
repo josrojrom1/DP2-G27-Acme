5ee4475798df8e25fd224d4bb2c9a760d8e48a52
josrojrom1 <josrojrom1@alum.us.es>
2024-02-20 12:07:32 +0100

    Auditor rol and credentials for login OK!

---
+++ src/main/java/acme/roles/Auditor.java
@@ -0,0 +1,40 @@
+
+package acme.roles;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractRole;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Auditor extends AbstractRole {
+
+	private static final long	serialVersionUID	= 1L;
+
+	//firm (not blank, shorter than 76 characters)
+	@NotBlank
+	@Length(max = 75)
+	private String				firm;
+
+	//professional ID (not blank, shorter than 26 characters)
+	@NotBlank
+	@Length(max = 26)
+	private String				professionalId;
+
+	//a list of certifications (not blank, shorter than 101 characters)
+	@NotBlank
+	@Length(max = 100)
+	private String				certifications;
+
+	//optional link with further information.
+	@URL
+	private String				link;
+
+}
