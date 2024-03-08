15540c2c0f0e6469bca65ec825604c4c7050669f
josrojrom1 <josrojrom1@alum.us.es>
2024-03-08 16:29:32 +0100

    Dashboards de auditor y admin actualizados con tipos primitivos OK!

--- src/main/java/acme/forms/AdministratorDashboard.java
+++ src/main/java/acme/forms/AdministratorDashboard.java
@@ -7,28 +7,28 @@ import acme.client.data.AbstractForm;
 
 public class AdministratorDashboard extends AbstractForm {
 
-	private static final long	serialVersionUID	= 1L;
+	private static final long		serialVersionUID	= 1L;
 
 	//total number of principals with each rol
-	Map<String, Integer>		totalNumOfPrincipal;
+	private Map<String, Integer>	totalNumOfPrincipal;
 
 	//ratio of notices with both an email address and a link
-	double						ratioOfNotices;
+	private double					ratioOfNotices;
 
 	//ratios of critical and non-critical objectives
-	double						ratioOfCriticalObjectives;
-	double						ratioOfNonCriticalObjectives;
+	private double					ratioOfCriticalObjectives;
+	private double					ratioOfNonCriticalObjectives;
 
 	//average, deviation, minimum, and maximum number of the value in the risks
-	double						riskValueAverage;
-	double						riskValueDeviation;
-	double						riskValueMinimum;
-	double						riskValueMaximum;
+	private double					riskValueAverage;
+	private double					riskValueDeviation;
+	private double					riskValueMinimum;
+	private double					riskValueMaximum;
 
 	//average, deviation, minimum, and maximum time of the number of claims posted over the last 10 weeks
-	double						numberOfClaimsAverage;
-	double						numberOfClaimsDeviation;
-	double						numberOfClaimsMinimum;
-	double						numberOfClaimsMaximum;
+	private double					numberOfClaimsAverage;
+	private double					numberOfClaimsDeviation;
+	private double					numberOfClaimsMinimum;
+	private double					numberOfClaimsMaximum;
 
 }
