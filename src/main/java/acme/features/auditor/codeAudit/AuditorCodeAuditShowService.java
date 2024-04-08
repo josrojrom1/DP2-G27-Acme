b5b89b7f953f776611bbe4ccd08603350142aa90
josrojrom1 <josrojrom1@alum.us.es>
2024-04-08 20:51:08 +0200

    CodeAuditShow muestra mark OK!

--- src/main/java/acme/features/auditor/codeAudit/AuditorCodeAuditShowService.java
+++ src/main/java/acme/features/auditor/codeAudit/AuditorCodeAuditShowService.java
@@ -1,12 +1,21 @@
 
 package acme.features.auditor.codeAudit;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import acme.client.data.models.Dataset;
 import acme.client.services.AbstractService;
+import acme.entities.audits.AuditRecord;
 import acme.entities.audits.CodeAudit;
+import acme.entities.audits.Mark;
 import acme.roles.Auditor;
 
 @Service
@@ -45,10 +54,10 @@ public class AuditorCodeAuditShowService extends AbstractService<Auditor, CodeAu
 	public void unbind(final CodeAudit object) {
 		assert object != null;
 
-		//int employerId;
-		//Collection<Company> contractors;
 		//SelectChoices choices;
 		Dataset dataset;
+		Mark auditMark;
+		auditMark = this.getModeOfAuditRecordMarks(object.getId());
 
 		//if (!object.isDraftMode())
 		//	contractors = this.repository.findAllContractors();
@@ -59,12 +68,32 @@ public class AuditorCodeAuditShowService extends AbstractService<Auditor, CodeAu
 		//choices = SelectChoices.from(contractors, "name", object.getContractor());
 
 		dataset = super.unbind(object, "code", "execution", "type", "correctiveActions", "mark", "link", "draftMode");
+		if (this.repository.findAuditRecordsById(object.getId()).isEmpty())
+			dataset.put("mark", " - ");
+		else
+			dataset.put("mark", auditMark);
+
 		dataset.put("draftMode", object.isDraftMode());
 
 		//dataset.put("contractor", choices.getSelected().getKey());
 		//dataset.put("contractors", choices);
 
 		super.getResponse().addData(dataset);
+	}
+
+	private Mark getModeOfAuditRecordMarks(final int codeAuditId) {
+		Map<Mark, Integer> markCount = new HashMap<>();
+		Mark[] marks = Mark.values();
+		Collection<AuditRecord> audRec = this.repository.findAuditRecordsById(codeAuditId);
+
+		for (Mark mark : marks) {
+			int count = (int) audRec.stream().filter(x -> x.getMark() == mark).count();
+			markCount.put(mark, count);
+		}
+		int maxCount = Collections.max(markCount.values());
+
+		List<Mark> maxCountList = markCount.entrySet().stream().filter(x -> x.getValue() == maxCount).sorted((a, b) -> a.getKey().compareTo(b.getKey())).map(Map.Entry::getKey).collect(Collectors.toList());
+		return maxCountList.get(0);
 	}
 
 }
