b19248c363cc03cfce7f682b79439674866bd7e1
josrojrom1 <josrojrom1@alum.us.es>
2024-05-11 12:43:02 +0200

    Arreglado bug que borraba la nota de un codeAudit al publicar/actualizar con errores OK!

--- src/main/java/acme/features/auditor/codeAudit/AuditorCodeAuditPublishService.java
+++ src/main/java/acme/features/auditor/codeAudit/AuditorCodeAuditPublishService.java
@@ -2,6 +2,11 @@
 package acme.features.auditor.codeAudit;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -88,6 +93,8 @@ public class AuditorCodeAuditPublishService extends AbstractService<Auditor, Cod
 
 		Collection<Project> projects;
 		Collection<AuditRecord> auditRecords;
+		Mark auditMark;
+		auditMark = this.getModeOfAuditRecordMarks(object.getId());
 		SelectChoices projectChoices;
 		SelectChoices typeChoices;
 		typeChoices = SelectChoices.from(Type.class, object.getType());
@@ -95,6 +102,10 @@ public class AuditorCodeAuditPublishService extends AbstractService<Auditor, Cod
 		projectChoices = SelectChoices.from(projects, "title", object.getProject());
 		Dataset dataset;
 		dataset = super.unbind(object, "code", "execution", "correctiveActions", "type", "mark", "link", "draftMode");
+		if (this.repository.findAuditRecordsById(object.getId()).isEmpty())
+			dataset.put("mark", " - ");
+		else
+			dataset.put("mark", auditMark);
 		dataset.put("project", projectChoices.getSelected().getKey());
 		dataset.put("projects", projectChoices);
 		dataset.put("type", typeChoices.getSelected().getKey());
@@ -109,6 +120,20 @@ public class AuditorCodeAuditPublishService extends AbstractService<Auditor, Cod
 				auditRecordsDraftModeState = false;
 		dataset.put("auditRecordsDraftModeState", auditRecordsDraftModeState);
 		super.getResponse().addData(dataset);
+	}
+	private Mark getModeOfAuditRecordMarks(final int codeAuditId) {
+		Map<Mark, Integer> markCount = new HashMap<>();
+		Mark[] marks = Mark.values();
+		Collection<AuditRecord> audRec = this.repository.findAuditRecordsById(codeAuditId);
+
+		for (Mark mark : marks) {
+			int count = (int) audRec.stream().filter(x -> x.getMark() == mark).count();
+			markCount.put(mark, count);
+		}
+		int maxCount = Collections.max(markCount.values());
+
+		List<Mark> maxCountList = markCount.entrySet().stream().filter(x -> x.getValue() == maxCount).sorted((a, b) -> a.getKey().compareTo(b.getKey())).map(Map.Entry::getKey).collect(Collectors.toList());
+		return maxCountList.get(0);
 	}
 
 }
--- src/main/java/acme/features/auditor/codeAudit/AuditorCodeAuditUpdateService.java
+++ src/main/java/acme/features/auditor/codeAudit/AuditorCodeAuditUpdateService.java
@@ -2,6 +2,11 @@
 package acme.features.auditor.codeAudit;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +16,7 @@ import acme.client.services.AbstractService;
 import acme.client.views.SelectChoices;
 import acme.entities.audits.AuditRecord;
 import acme.entities.audits.CodeAudit;
+import acme.entities.audits.Mark;
 import acme.entities.audits.Type;
 import acme.entities.projects.Project;
 import acme.roles.Auditor;
@@ -87,6 +93,9 @@ public class AuditorCodeAuditUpdateService extends AbstractService<Auditor, Code
 
 		Collection<Project> projects;
 		Collection<AuditRecord> auditRecords;
+		Mark auditMark;
+
+		auditMark = this.getModeOfAuditRecordMarks(object.getId());
 
 		SelectChoices projectChoices;
 		SelectChoices typeChoices;
@@ -95,6 +104,11 @@ public class AuditorCodeAuditUpdateService extends AbstractService<Auditor, Code
 		projectChoices = SelectChoices.from(projects, "title", object.getProject());
 		Dataset dataset;
 		dataset = super.unbind(object, "code", "execution", "correctiveActions", "type", "link", "draftMode");
+		if (this.repository.findAuditRecordsById(object.getId()).isEmpty())
+			dataset.put("mark", " - ");
+		else
+			dataset.put("mark", auditMark);
+
 		dataset.put("project", projectChoices.getSelected().getKey());
 		dataset.put("projects", projectChoices);
 		dataset.put("type", typeChoices.getSelected().getKey());
@@ -111,6 +125,20 @@ public class AuditorCodeAuditUpdateService extends AbstractService<Auditor, Code
 				auditRecordsDraftModeState = false;
 		dataset.put("auditRecordsDraftModeState", auditRecordsDraftModeState);
 		super.getResponse().addData(dataset);
+	}
+	private Mark getModeOfAuditRecordMarks(final int codeAuditId) {
+		Map<Mark, Integer> markCount = new HashMap<>();
+		Mark[] marks = Mark.values();
+		Collection<AuditRecord> audRec = this.repository.findAuditRecordsById(codeAuditId);
+
+		for (Mark mark : marks) {
+			int count = (int) audRec.stream().filter(x -> x.getMark() == mark).count();
+			markCount.put(mark, count);
+		}
+		int maxCount = Collections.max(markCount.values());
+
+		List<Mark> maxCountList = markCount.entrySet().stream().filter(x -> x.getValue() == maxCount).sorted((a, b) -> a.getKey().compareTo(b.getKey())).map(Map.Entry::getKey).collect(Collectors.toList());
+		return maxCountList.get(0);
 	}
 
 }
