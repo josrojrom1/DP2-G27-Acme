7497d9570df838f0afd661e2e5c6149246745213
DanielAlors <daniel03.alors@gmail.com>
2024-05-26 16:55:45 +0200

    Cambios

--- src/main/java/acme/features/developer/trainingModule/DeveloperTrainingModuleUpdateService.java
+++ src/main/java/acme/features/developer/trainingModule/DeveloperTrainingModuleUpdateService.java
@@ -78,14 +78,13 @@ public class DeveloperTrainingModuleUpdateService extends AbstractService<Develo
 			super.state(existing == null, "code", "developer.training-module.form.error.code.duplicated");
 		}
 
-		if (!super.getBuffer().getErrors().hasErrors("creationMoment"))
-			super.state(object.getCreationMoment() != null, "creationMoment", "developer.training-module.form.error.NotcreationMoment");
-
 		if (!super.getBuffer().getErrors().hasErrors("creationMoment")) {
 			super.state(MomentHelper.isAfterOrEqual(object.getCreationMoment(), baseDate), "creationMoment", "developer.training-module.form.error.tooLittle");
 			super.state(MomentHelper.isBeforeOrEqual(object.getCreationMoment(), topDate), "creationMoment", "developer.training-module.form.error.tooBig");
 		}
 		if (object.getUpdateMoment() != null) {
+			if (!super.getBuffer().getErrors().hasErrors("updateMoment"))
+				super.state(object.getCreationMoment() != null, "updateMoment", "developer.training-module.form.error.NotcreationMoment");
 			if (!super.getBuffer().getErrors().hasErrors("updateMoment")) {
 				super.state(MomentHelper.isAfterOrEqual(object.getUpdateMoment(), baseDate), "updateMoment", "developer.training-module.form.error.tooLittle");
 				super.state(MomentHelper.isBeforeOrEqual(object.getUpdateMoment(), topDate), "updateMoment", "developer.training-module.form.error.tooBig");
@@ -93,6 +92,11 @@ public class DeveloperTrainingModuleUpdateService extends AbstractService<Develo
 			if (object.getCreationMoment() != null)
 				if (!super.getBuffer().getErrors().hasErrors("updateMoment"))
 					super.state(object.getUpdateMoment() == null || object.getUpdateMoment().after(object.getCreationMoment()), "updateMoment", "developer.training-module.form.error.updateMoment");
+		}
+		//Para evitar hacking post desde Postman
+		if (!super.getBuffer().getErrors().hasErrors("project")) {
+			Project objProject = object.getProject();
+			super.state(!objProject.isDraftMode(), "project", "developer.training-module.form.error.code.projectNotPublish");
 		}
 	}
 
