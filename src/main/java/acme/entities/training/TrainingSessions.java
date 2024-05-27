1b7676ea86678059f542c93d7666742560b26450
agude <agude@DESKTOP-PQGQI9Q>
2024-05-27 18:31:03 +0200

    cambios y docs S3

--- src/main/java/acme/entities/training/TrainingSessions.java
+++ src/main/java/acme/entities/training/TrainingSessions.java
@@ -5,7 +5,9 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.Valid;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -22,6 +24,9 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(indexes = {
+	@Index(columnList = "code")
+})
 public class TrainingSessions extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
